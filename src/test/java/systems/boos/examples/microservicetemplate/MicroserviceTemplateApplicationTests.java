f51169edfb109d420522eaf90c027215e722d89a
Stefan Boos <sboos@dimedis.de>
2019-06-03 08:23:51 +0200

    initial version created by start.spring.io

---
+++ src/test/java/systems/boos/examples/microservicetemplate/MicroserviceTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package systems.boos.examples.microservicetemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceTemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
