9d16568dd459197d14d101ac503a24988291df62
Stefan Boos <stefan.boos@gmx.de>
2019-06-04 05:42:25 +0200

    feat (qa-tests): include running the qa-tests into the build

--- core/src/test/java/systems/boos/examples/microservicetemplate/ApplicationTest.java
+++ core/src/test/java/systems/boos/examples/microservicetemplate/ApplicationTest.java
@@ -5,10 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SuppressWarnings("NewClassNamingConvention")
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests
+public class ApplicationTest
 {
 
 	@Test
---
+++ qa-tests/src/test/java/systems/boos/examples/microservicetemplate/acceptancetest/ActuatorEndpointTest.java
@@ -0,0 +1,14 @@
+package systems.boos.examples.microservicetemplate.acceptancetest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ActuatorEndpointTest
+{
+   @Test
+   public void test()
+   {
+      assertTrue("test failed", true);
+   }
+}
